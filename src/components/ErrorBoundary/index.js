9526f850c16c0d114f43f854055c4f5a8769aec7
leoni <leonimega@hotmail.com>
2017-12-28 22:40:56 -0200

    first commit

---
+++ src/components/ErrorBoundary/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  componentDidCatch(error, info) {
+    this.setState({ hasError: true });
+    // You can also log the error to an error reporting service
+    console.log({error, info});
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (<h1>Something went wrong.</h1>);
+    }
+    return this.props.children;
+  }
+}
+
+ErrorBoundary.propTypes = {
+  children: PropTypes.element
+};
+
+export default ErrorBoundary;
