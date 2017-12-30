724a42d2cddd50e6a2a936681e5a53081cd8222a
leoni <leonimega@hotmail.com>
2017-12-30 19:28:14 -0200

    add more content to AboutSection component and create new utility classes

--- src/components/AboutSection/index.js
+++ src/components/AboutSection/index.js
@@ -5,9 +5,19 @@ class AboutSection extends Component {
   render() {
     return (
       <section className="section-about">
-        <h2 className="heading-secondary">
-          Exciting tours for adventurous people
-        </h2>
+        <div className="u-center-text u-margin-bottom-8">
+          <h2 className="heading-secondary">
+            Exciting tours for adventurous people
+          </h2>
+        </div>
+        <div className="row">
+          <div className="col-1-of-2">
+            Text content
+          </div>
+          <div className="col-1-of-2">
+            Image composition
+          </div>
+        </div>
       </section>
     );
   }
