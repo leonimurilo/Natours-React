4c6941fd90290c2eb2fd1cdf8a2eb7d7e9532a8d
leoni <leonimega@hotmail.com>
2017-12-30 21:08:12 -0200

    add more stuff in AboutSection component

--- src/components/AboutSection/index.js
+++ src/components/AboutSection/index.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
+import Button from "../Button";
 
 class AboutSection extends Component {
 
   render() {
     return (
       <section className="section-about">
-        <div className="u-center-text u-margin-bottom-8">
+        <div className="u-center-text u-margin-bottom-big">
           <h2 className="heading-secondary">
             Exciting tours for adventurous people
           </h2>
         </div>
         <div className="row">
           <div className="col-1-of-2">
-            Text content
+            <h3 className="heading-tertiary u-margin-bottom-small">
+              You're going to fall in love with nature
+            </h3>
+            <p className="paragraph">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum quisquam facilis architecto commodi nesciunt dicta doloribus, odit nam iste voluptatum, repellendus officia.
+            </p>
+            <h3 className="heading-tertiary u-margin-bottom-small">
+              Live adventures like you never have before
+            </h3>
+            <p className="paragraph">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque adipisci accusantium fuga sed molestias ipsam, culpa, nobis quod cum asperiores rem quibusdam earum quo tempore omnis doloribus dignissimos unde perspiciatis?
+            </p>
+            <Button href="#" styleName="btn-text">Learn more →</Button>
           </div>
           <div className="col-1-of-2">
             Image composition
