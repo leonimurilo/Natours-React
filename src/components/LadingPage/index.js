d920a71d4bf95a0685fdb2b1a93c83075303255a
leoni <leonimega@hotmail.com>
2017-12-29 23:00:33 -0200

    add GridExample component and some notes

--- src/components/LadingPage/index.js
+++ src/components/LadingPage/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from "../Header";
+import GridExample from "../GridExample";
 
 class LadingPage extends Component {
 
@@ -7,6 +8,7 @@ class LadingPage extends Component {
     return (
       <div className="lading-page">
         <Header/>
+        <GridExample/>
       </div>
     );
   }
