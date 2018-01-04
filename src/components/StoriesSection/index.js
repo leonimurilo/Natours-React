39a2f30a41e57b1486b13747c75056e9ad324321
leoni <leonimega@hotmail.com>
2018-01-04 21:18:06 -0200

    add background video to the storiesSection

--- src/components/StoriesSection/index.js
+++ src/components/StoriesSection/index.js
@@ -5,11 +5,21 @@ import Button from "../Button";
 import photo1 from "../../assets/images/nat-8.jpg";
 import photo2 from "../../assets/images/nat-9.jpg";
 
+import mp4Video from "../../assets/videos/video.mp4";
+import webmVideo from "../../assets/videos/video.webm";
+
 class StoriesSection extends Component {
 
   render() {
     return (
       <section className="section-stories">
+        <div className="bg-video">
+          <video className="bg-video__content" autoPlay muted loop>
+            <source src={mp4Video} type="video/mp4"/>
+            <source src={webmVideo} type="video/webm"/>
+            Your browser does not support the video tag.
+          </video>
+        </div>
         <div className="u-center-text u-margin-bottom-big">
           <h2 className="heading-secondary"></h2>
         </div>
