85755a77e70f089d3048df381bb6c95ce80ba892
leoni <leonimega@hotmail.com>
2018-01-04 20:07:00 -0200

    add second story and button to Stories section

--- src/components/StoriesSection/index.js
+++ src/components/StoriesSection/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import Story from "../Story";
+import Button from "../Button";
 
 import photo1 from "../../assets/images/nat-8.jpg";
+import photo2 from "../../assets/images/nat-9.jpg";
 
 class StoriesSection extends Component {
 
@@ -16,8 +18,19 @@ class StoriesSection extends Component {
           imageUrl={photo1}
           imageAlt="Person on a tour"
           caption="Mary Smith">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          </Story>
+        </div>
+        <div className="row">
+          <Story header="Wow! My life is completely different now"
+          imageUrl={photo2}
+          imageAlt="Person on a tour"
+          caption="Jack wilson">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id optio nesciunt dolorum Id optio nesciunt dolorum Id optio nesciunt dolorum provident ducimus unde illo voluptate possimus ipsum, quaerat, natus modi autem tempora. Deserunt sed veniam autem voluptatem, reiciendis!
           </Story>
+        </div>
+        <div className="u-center-text u-margin-top-large">
+          <Button href="#" className="btn-text">Read all stories →</Button>
         </div>
       </section>
     );
