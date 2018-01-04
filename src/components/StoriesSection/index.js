68006eb1b1fa88a7e1f49759c8fced4c8fc80df3
leoni <leonimega@hotmail.com>
2018-01-04 19:18:23 -0200

    add story section

--- src/components/StoriesSection/index.js
+++ src/components/StoriesSection/index.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import Story from "../Story";
+
+import photo1 from "../../assets/images/nat-8.jpg";
 
 class StoriesSection extends Component {
 
@@ -9,7 +12,11 @@ class StoriesSection extends Component {
           <h2 className="heading-secondary"></h2>
         </div>
         <div className="row">
-          
+          <Story header="I had the best week ever with my family"
+          imageUrl={photo1}
+          imageAlt="Person on a tour">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id optio nesciunt dolorum Id optio nesciunt dolorum Id optio nesciunt dolorum provident ducimus unde illo voluptate possimus ipsum, quaerat, natus modi autem tempora. Deserunt sed veniam autem voluptatem, reiciendis!
+          </Story>
         </div>
       </section>
     );
