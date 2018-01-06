f3d498c6aa15b39bc592d45928c07dcc9361bc95
leoni <leonimega@hotmail.com>
2018-01-06 12:43:51 -0200

    finish form

--- src/components/BookSection/index.js
+++ src/components/BookSection/index.js
@@ -22,17 +22,26 @@ class BookSection extends Component {
                   <input id="email" type="email" className="form__input" placeholder="Email address" required/>
                   <label htmlFor="email" className="form__label">Email address</label>
                 </div>
-                <div className="form__group">
+                <div className="form__group u-margin-bottom-medium ">
+
                   <div className="form__radio-group">
                     <input type="radio" className="form__radio-input" id="smallTour" name="size"/>
-                    <label htmlFor="smallTour" className="form__radio-label">Small tour group</label>
+                    <label htmlFor="smallTour" className="form__radio-label">
+                      <span className="form__radio-button"></span>
+                      Small tour group
+                    </label>
+                  </div>
+
+                  <div className="form__radio-group">
+                    <input type="radio" className="form__radio-input" id="largeTour" name="size"/>
+                    <label htmlFor="largeTour" className="form__radio-label">
+                      <span className="form__radio-button"></span>
+                      Large tour group
+                    </label>
                   </div>
                 </div>
                 <div className="form__group">
-                  <div className="form__radio-group">
-                    <input type="radio" className="form__radio-input" id="largeTour" name="size"/>
-                    <label htmlFor="smallTour" className="form__radio-label">Large tour group</label>
-                  </div>
+                  <button className="btn btn--green">Next step →</button>
                 </div>
               </form>
             </div>
