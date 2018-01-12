bfcf3e872b5e74dc6b6bb3899e57bcb769f5d208
leoni <leonimega@hotmail.com>
2018-01-12 19:52:12 -0200

    add spans to number navigation items

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -10,11 +10,11 @@ class Navigation extends Component {
         <div className="navigation__background"> </div>
         <nav className="navigation__nav">
           <ul className="navigation__list">
-            <li className="navigation__item"><a href="#" className="navigation__link">About Natous</a></li>
-            <li className="navigation__item"><a href="#" className="navigation__link">Your benefits</a></li>
-            <li className="navigation__item"><a href="#" className="navigation__link">Popular tours</a></li>
-            <li className="navigation__item"><a href="#" className="navigation__link">Stories</a></li>
-            <li className="navigation__item"><a href="#" className="navigation__link">Book now</a></li>
+            <li className="navigation__item"><a href="#" className="navigation__link"><span>01</span>About Natous</a></li>
+            <li className="navigation__item"><a href="#" className="navigation__link"><span>02</span>Your benefits</a></li>
+            <li className="navigation__item"><a href="#" className="navigation__link"><span>03</span>Popular tours</a></li>
+            <li className="navigation__item"><a href="#" className="navigation__link"><span>04</span>Stories</a></li>
+            <li className="navigation__item"><a href="#" className="navigation__link"><span>05</span>Book now</a></li>
           </ul>
         </nav>
       </div>
