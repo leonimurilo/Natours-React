c0af13daa4795d5a40bc2c9476622d42d4d2d516
leoni <leonimega@hotmail.com>
2018-01-12 20:59:31 -0200

    add hamburguer icon to navigation menu

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -6,7 +6,9 @@ class Navigation extends Component {
     return (
       <div className="navigation">
         <input type="checkbox" className="navigation__checkbox" id="navi-toggle"/>
-        <label htmlFor="navi-toggle" className="navigation__button">Menu</label>
+        <label htmlFor="navi-toggle" className="navigation__button">
+          <span className="navigation__icon"></span>
+        </label>
         <div className="navigation__background"> </div>
         <nav className="navigation__nav">
           <ul className="navigation__list">
