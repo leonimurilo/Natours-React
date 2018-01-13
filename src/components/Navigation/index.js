3f5851fdb4ccf131e4e1db5a77c6a759aee1d55b
leoni <leonimega@hotmail.com>
2018-01-13 01:18:03 -0200

    close nav button when chaging route via menu

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -3,17 +3,33 @@ import {Link} from "react-router-dom";
 
 class Navigation extends Component {
 
+  constructor(props){
+    super(props)
+    this.state = {isOpen: false};
+  }
+
+  toggleMenu(event){
+    // event.preventDefault();
+    this.setState({isOpen: !this.state.isOpen})
+  }
+
   renderOptions(menuOptions){
-    return menuOptions.map(function(element, index, array){
-      return (<li key={index} className="navigation__item"><Link to={element.to} className="navigation__link"><span>0{index + 1}</span>{element.text}</Link></li>);
+    return menuOptions.map((element, index) =>{
+      return (
+        <li key={index} className="navigation__item">
+          <Link to={element.to} onClick={this.toggleMenu.bind(this)} className="navigation__link">
+            <span>0{index + 1}</span>{element.text}
+          </Link>
+        </li>
+      );
     });
   }
 
   render() {
     return (
       <div className="navigation">
-        <input type="checkbox" className="navigation__checkbox" id="navi-toggle"/>
-        <label htmlFor="navi-toggle" className="navigation__button">
+        <input type="checkbox" className="navigation__checkbox" id="navi-toggle" checked={this.state.isOpen}/>
+        <label htmlFor="navi-toggle" className="navigation__button" onClick={this.toggleMenu.bind(this)}>
           <span className="navigation__icon"></span>
         </label>
         <div className="navigation__background"> </div>
